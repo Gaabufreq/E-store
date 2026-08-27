1777538c00f92e6a457cab4d051a863f00458f6d
Neelambuj Tiwari <178289161+Gaabufreq@users.noreply.github.com>
2026-08-27 19:03:44 +0530

    Updated live backend and frontend URLs

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://e-store-yp7z.onrender.com",
   withCredentials: true, // Cookies pass karne ke liye zaroori hai
   headers: {
     "Content-Type": "application/json",
